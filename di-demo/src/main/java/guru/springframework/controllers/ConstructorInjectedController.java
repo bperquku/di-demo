c05a664fca784af79eda299cba769f32cc4bd1b6
bperquku <bperquku@gmail.com>
2018-09-19 15:39:39 +0200

    spring-di

--- di-demo/src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ di-demo/src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -1,11 +1,16 @@
 package guru.springframework.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.services.GreetingService;
 
+@Controller
 public class ConstructorInjectedController {
 
 	private GreetingService greetingService;
 
+	@Autowired
 	public ConstructorInjectedController(GreetingService greetingService) {
 		this.greetingService = greetingService;
 	}
--- di-demo/src/main/java/guru/springframework/controllers/SetterInjectedController.java
+++ di-demo/src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -1,9 +1,14 @@
 package guru.springframework.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.services.GreetingService;
 
+@Controller
 public class SetterInjectedController {
 
+	@Autowired
 	private GreetingService greetingService;
 
 	public String sayHello() {
