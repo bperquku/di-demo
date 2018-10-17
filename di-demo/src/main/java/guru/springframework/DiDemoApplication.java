a93497c34e1a484d275d0c075cdc948ff5012ea6
bperquku <bperquku@gmail.com>
2018-10-17 14:37:44 +0200

    property source impl

--- di-demo/src/main/java/guru/springframework/DiDemoApplication.java
+++ di-demo/src/main/java/guru/springframework/DiDemoApplication.java
@@ -6,18 +6,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import guru.springframework.controllers.*;
+import guru.springframework.examplebeans.FakeDataSource;
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"guru.services","guru.springframework"})
+@ComponentScan(basePackages = { "guru.services", "guru.springframework" })
 public class DiDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUser());
 	}
 }
